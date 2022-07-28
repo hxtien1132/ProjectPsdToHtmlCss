a48e641f5c8d7abb27b3b412b59a2d74abaa0d3c
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2022-07-28 13:45:06 +0700

    ad
    ad

--- explore-master/assets/js/search.js
+++ explore-master/assets/js/search.js
@@ -1,4 +1,4 @@
-const buytBtns = document.querySelectorAll('.js-search-ticket');
+const searchBtns = document.querySelectorAll('.js-search-ticket');
 const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.js-modal-close');
 const modalContainer = document.querySelector('.js-modal-container');
@@ -8,8 +8,8 @@ function showBuyTickets() {
 function hideBuyTickets() {
   modal.classList.remove('open');
 }
-for (const buyBtn of buytBtns) {
-  buyBtn.addEventListener('click', showBuyTickets);
+for (const searchBtn of searchBtns) {
+  searchBtn.addEventListener('click', showBuyTickets);
  
 
  }
